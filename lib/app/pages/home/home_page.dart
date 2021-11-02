ff74d529592d2181503a982b7c696dd71541d2c9
Firmino Neto <firmino-017@hotmail.com>
2021-11-02 17:17:19 -0300

    show points location

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -7,6 +7,8 @@ import 'package:engeselt_teste/app/pages/local_details/local_details.dart';
 import 'package:engeselt_teste/app/pages/saved_locals/saved_locals_page.dart';
 import 'package:engeselt_teste/app/store/local_store.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,11 +18,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late GoogleMapController mapController;
+  LatLng? _center;
+  Set<Marker> markers = {};
+
+  Future<void> getCurrentLocation() async {
+    final locData = await Location().getLocation();
+    _center = LatLng(locData.latitude!, locData.longitude!);
+    setState(() {});
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  loadMarker() async {
+    await Future.delayed(Duration(seconds: 2));
+    for (var local in localStore.locals.value) {
+      markers.add(Marker(
+        position: local.latLng!,
+        infoWindow: InfoWindow(title: local.description),
+        markerId: MarkerId(local.id.toString()),
+      ));
+    }
+
+    print('TAMANHOS');
+    print(localStore.locals.value.length);
+    print(markers.length);
+    setState(() {});
+  }
+
   LocalStore localStore = LocalStore();
+
+  @override
+  void initState() {
+    loadMarker();
+    getCurrentLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       appBar: AppBar(
         title: Text('Pontos de interesse'),
         actions: [
@@ -35,7 +75,12 @@ class _HomePageState extends State<HomePage> {
               icon: Icon(Icons.map))
         ],
       ),
-      body: Container(),
+      body: _center == null
+          ? Center(child: Text('Carregando'))
+          : GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(zoom: 11, target: _center!),
+              markers: markers),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
